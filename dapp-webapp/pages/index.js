34614dc31c03044ee8f3f2fd7e6db6532a8d6cde
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-10 12:30:21 +0800

    dded Get and Set Message UI on Frontend

--- dapp-webapp/pages/index.js
+++ dapp-webapp/pages/index.js
@@ -6,13 +6,26 @@ import Message from "../../dapp-contracts/artifacts/contracts/Message.sol/Messag
 const messageContractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 export default function Home() {
-  const [message, setmessage] = useState("");
+  let [message, setmessage] = useState(""); //get
+  let [newMessage, setNewMessage] = useState(""); //set
+  const [account, setAccount] = useState("");
+
   useEffect(() => {
-    fetchMessage();
-    setMessage();
+    getAccount();
+    getMessage();
   }, []);
 
-  async function fetchMessage() {
+  async function getAccount() {
+    if (typeof window.ethereum !== "undefined") {
+      // Wait until the user logsin to metamask to proceed, get account)
+      const [acc] = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(acc);
+    }
+  }
+
+  async function getMessage() {
     // Check if Metamask is installed(injected at window.Ethereum)
     if (typeof window.ethereum !== "undefined") {
       // Connect to blockchain via Web3Provider since its in browser
@@ -26,7 +39,8 @@ export default function Home() {
 
       try {
         const data = await messageContract.getMessage();
-        console.log(data);
+        setmessage(data);
+        console.log(message);
       } catch (error) {
         console.log(error);
       }
@@ -48,19 +62,31 @@ export default function Home() {
 
       try {
         // Pop-ups Metamask evry transaction to be signed
-        const transaction = await messageContract.setMessage("new");
+        const transaction = await messageContract.setMessage(newMessage);
         // wait until its mined
         await transaction.wait();
       } catch (error) {
         console.log("tx cancelled");
       }
-      fetchMessage();
+      setNewMessage("");
+      getMessage();
     }
   }
 
   return (
     <div>
-      <h1></h1>
+      <h4>Account: {account}</h4>
+      <h1>Message from Contract: {message}</h1>
+
+      <button onClick={getMessage}>Get New Message</button>
+      <br />
+      <br />
+      <input
+        type="text"
+        onChange={(e) => setNewMessage(e.target.value)}
+        placeholder="Your new message here"
+      />
+      <button onClick={setMessage}>Set New Message</button>
     </div>
   );
 }
