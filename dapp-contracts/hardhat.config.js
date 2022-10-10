9035cbead327ae8e7c10031f310edfb57bc72a38
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-10-10 21:09:31 +0800

    Added Mumbai and Matic Networks

--- dapp-contracts/hardhat.config.js
+++ dapp-contracts/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/v1/${process.env.MATICVIGIL_APP_ID}`,
+      accounts: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
+    },
+    matic: {
+      url: `https://rpc-mainnet.maticvigil.com/v1/${process.env.MATICVIGIL_APP_ID}`,
+      accounts: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
+    },
+  },
 };
